22e6b373f43ce7e3bda9d26a935e3894864a06cb
cc <yamatagit@163.com>
2021-09-06 09:22:49 +0800

    提交初始框架

---
+++ twenty-three/src/main/java/com/cc/twentythree/TwentyThreeApplication.java
@@ -0,0 +1,13 @@
+package com.cc.twentythree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwentyThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwentyThreeApplication.class, args);
+    }
+
+}
---
+++ twenty-three/src/test/java/com/cc/twentythree/TwentyThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.twentythree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwentyThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
